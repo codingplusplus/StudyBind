39334dfaf9a76ad5aee73d4e282640bf978d613d
chenchao <8206503@qq.com>
2015-08-06 09:46:47 +0800

    Initial commit

---
+++ app/src/main/java/study/chenchao/studybind/User.java
@@ -0,0 +1,29 @@
+package study.chenchao.studybind;
+
+/**
+ * Created by chenchao on 15/8/5.
+ */
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
